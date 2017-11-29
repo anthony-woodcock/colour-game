24cf710fd3b32f3a074da0605bc8f69fd312c484
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-11-29 18:38:17 +0000

    assigned colour to each square, if user clicks square with correct RGB alerts correct, alerts wrong if not

--- colourGame.js
+++ colourGame.js
@@ -8,7 +8,24 @@ var colours = [
 ]
 
 var squares = document.querySelectorAll(".square")
+var pickedColour = colours[3]
+var colourDisplay = document.getElementById("colourDisplay")
+
+colourDisplay.textContent = pickedColour
 
 for(var i = 0; i < squares.length; i++) {
+    //add initial colours to squares
     squares[i].style.backgroundColor = colours[i]
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+        //grab colour of clicked square
+        var clickedColour = this.style.backgroundColor
+        //compare colour to pickedColour
+        if(clickedColour === pickedColour){
+            alert("Correct!")
+        } else {
+            alert("Wrong!")
+        }
+    })
 }
