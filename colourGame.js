5f22182a1b9a3d1d545ac4242c3bb011a21bd1e7
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-09 12:20:24 +0000

    correct message disapears when a new game starts

--- colourGame.js
+++ colourGame.js
@@ -46,6 +46,8 @@ resetButton.addEventListener('click',function(){
     pickedColour = pickColour()
     //change colour display to match picked colour
     colourDisplay.textContent = pickedColour
+
+    messagesDisplay.textContent = ""
     //change colours of squares
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colours[i]
